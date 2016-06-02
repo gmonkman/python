cb213e23426618f2132fc2f8b03f2599a9bd6c88
Graham Monkman <gmonkman@mistymountains.biz>
2016-06-02 14:50:13 +0100

    Ready to use
    work complete for now

--- permanal/main.py
+++ permanal/main.py
@@ -4,7 +4,11 @@ of my FMM and PAM data with my point coincident data
 by 1km^2 grids.
 Kendall's correlation coefficent is used
 '''
+
+#base
 import csv
+
+#third party
 import scipy
 import scipy.stats
 
@@ -14,6 +18,7 @@ import funclib
 import iolib
 
 ITERATIONS = funclib.read_number(raw_input('Input iterations:'), 1000)
+EXCLUDE_ZEROS = bool(funclib.read_number(raw_input("Exclude zero pairs (0 = include zero pairs, 1 = exclude zero pairs):"), 0))
 MY_DATA = open('data\\all.csv', 'rb')
 DICT_READER = csv.DictReader(MY_DATA, fieldnames=csv.reader(MY_DATA).next())
 SCORES = []
@@ -21,6 +26,8 @@ FMM_SCORES = []
 FMM_YEAR = []
 PAM_SCORES = []
 PAM_DAYS = []
+
+
 try:
     for row in DICT_READER:
         SCORES.append(float(row['Scores']))
@@ -41,11 +48,11 @@ FMM_CORRS = [['tau', 'p']]
 
 TAU, CONF = scipy.stats.kendalltau(PAM_SCORES, PAM_DAYS)
 PAM_CORRS.append([TAU, CONF])
-PAM_CORRS.extend(statslib.permuted_correlation(PAM_SCORES, PAM_DAYS, ITERATIONS))
+PAM_CORRS.extend(statslib.permuted_correlation(PAM_SCORES, PAM_DAYS, ITERATIONS, exclude_zero_pairs=EXCLUDE_ZEROS))
 
 TAU, CONF = scipy.stats.kendalltau(FMM_SCORES, FMM_YEAR)
 FMM_CORRS.append([TAU, CONF])
-FMM_CORRS.extend(statslib.permuted_correlation(FMM_SCORES, FMM_YEAR, ITERATIONS))
+FMM_CORRS.extend(statslib.permuted_correlation(FMM_SCORES, FMM_YEAR, ITERATIONS, exclude_zero_pairs=EXCLUDE_ZEROS))
 
 iolib.writecsv('data\\pam_results.csv', PAM_CORRS, inner_as_rows=False)
 iolib.writecsv('data\\fmm_results.csv', FMM_CORRS, inner_as_rows=False)
---
+++ permanal/test.py
@@ -0,0 +1,5 @@
+import funclib
+
+a=[1,0,0,3,4,0]
+b=[1,0,1,3,4,0]
+funclib.list_delete_value_pairs(a,b,0)
