4246645465e7343201c96ead5c1c3591408ac389
Graham Monkman <gmonkman@mistymountains.biz>
2017-03-21 20:04:45 +0000

    change settings

--- imgscrape/settings.py
+++ imgscrape/settings.py
@@ -18,13 +18,13 @@ IMAGES_MIN_WIDTH = 300
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 1
+CONCURRENT_REQUESTS = 4
 
 #Throttling
 DOWNLOAD_DELAY = 4
 RANDOMIZE_DOWNLOAD_DELAY = True
-#CONCURRENT_REQUESTS_PER_DOMAIN = 1
-#CONCURRENT_REQUESTS_PER_IP = 1
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
+CONCURRENT_REQUESTS_PER_IP = 1
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
@@ -59,14 +59,9 @@ RANDOMIZE_DOWNLOAD_DELAY = True
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
-# The initial download delay
 AUTOTHROTTLE_START_DELAY = 3
-# The maximum download delay to be set in case of high latencies
 AUTOTHROTTLE_MAX_DELAY = 30
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
 AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
