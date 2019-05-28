dfb72826f26d7677b307f55b2ea69df0f90016f2
Graham Monkman <gmonkman@mistymountains.biz>
2019-05-28 16:36:02 +0100

    work

--- requirements_dump.cmd
+++ requirements_dump.cmd
@@ -1,12 +1,16 @@
-@echo off
-pipreqs c:\development\python\pgnet --encoding=utf-8 --force
-pipreqs c:\development\python\plotlib --encoding=utf-8 --force
 pipreqs c:\development\python\dblib --encoding=utf-8 --force
 pipreqs c:\development\python\focalpermute --encoding=utf-8 --force
 pipreqs c:\development\python\funclib --encoding=utf-8 --force
+pipreqs c:\development\python\gazetteer --encoding=utf-8 --force
+pipreqs c:\development\python\gazetteerdb --encoding=utf-8 --force
+pipreqs c:\development\python\gis --encoding=utf-8 --force
 pipreqs c:\development\python\imagedb --encoding=utf-8 --force
 pipreqs c:\development\python\imgscrape --encoding=utf-8 --force
+pipreqs c:\development\python\mmo --encoding=utf-8 --force
+pipreqs c:\development\python\mmodb --encoding=utf-8 --force
+pipreqs c:\development\python\mmodb --encoding=utf-8 --force
+pipreqs c:\development\python\nlp --encoding=utf-8 --force
 pipreqs c:\development\python\opencvlib --encoding=utf-8 --force
 pipreqs c:\development\python\permanal --encoding=utf-8 --force
 pipreqs c:\development\python\pgnet --encoding=utf-8 --force
-pause
+pipreqs c:\development\python\plotlib --encoding=utf-8 --force
