ac32f94eaf3cb8490d337743c45099e994bcf642
Graham Monkman <gmonkman@mistymountains.biz>
2019-05-31 01:53:24 +0100

    work

--- imgscrape/settings_backup.py
+++ imgscrape/settings_backup.py
@@ -1,30 +1,45 @@
 # pylint: disable=C0103, too-few-public-methods, locally-disabled, unused-variable
 '''settings'''
+from funclib.iolib import wait_key
+import fuckit
 BOT_NAME = 'imgscrape'
 SPIDER_MODULES = ['imgscrape.spiders']
 NEWSPIDER_MODULE = 'imgscrape.spiders'
 
 
 
-#Item Pipelines
-#ITEM_PIPELINES = {'imgscrape.pipelines.UGCWriter': 10}
-#MSSQL_DUPLICATE_CHECK = True
+#SQL SERVER EXPORT
+ITEM_PIPELINES = {'imgscrape.pipelines.UGCWriter': 10}
+MSSQL_DUPLICATE_CHECK = False
 MIN_BODY_LENGTH = 30
 
-
+#IMAGE - DIDNT WORK
 #IMAGES_STORE = 'C:/development/python/imgscrape/images'
 #IMAGES_MIN_HEIGHT = 300
 #IMAGES_MIN_WIDTH = 300
 
+#FILE STORE
 #ITEM_PIPELINES = {'scrapy.pipelines.files.FilesPipeline': 1}
 #FILES_STORE = '/path/to/valid/dir'
 
 
+#EASY CSV FEED
+#ITEM_PIPELINES = {'scrapy.pipelines.files.FilesPipeline': 1}
+#FEED_FORMAT = 'csv'
+#FEED_URI = 'file:.c:/temp/test_feed.csv'
 
-#FEED
-FEED_FORMAT = 'csv'
-FEED_URI = 'file:.c:/temp/test_feed.csv'
-print('\n*** USING CSV FEED TO FILE %s ***\n' % FEED_URI)
+
+with fuckit:
+    if  ITEM_PIPELINES == {'imgscrape.pipelines.UGCWriter': 10}:
+        print('\nconnecting to mssql ....')
+        import mmodb as _
+        print('\nMSSQL:%s' %  _.ENGINE)
+    else:
+        print('\nFile:%s%s' % (FEED_URI, FILES_STORE))
+
+    if wait_key('\nPress Q to quit, or any other key to start\n').lower() == 'q':
+        exit()
+
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0'
 ROBOTSTXT_OBEY = True
