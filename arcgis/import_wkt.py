9e84cbb39e3022c493233e20025bf4251b74456f
gmonkman <gmonkman@mistymountains.biz>
2019-07-30 13:32:46 +0100

    work

--- arcgis/import_wkt.py
+++ arcgis/import_wkt.py
@@ -2,32 +2,37 @@ import arcpy
 
 sr = arcpy.SpatialReference(4326)
 
-fname = "C:\\Users\\Team\\Documents\\Theo Laptop Folder\\Tasks\\Quick tasks\\WKTtest\\WKT_to_QGISmakealayerCSV2.csv"
+fname = "C:/temp/facilities.txt"
 
-field1 = "WKT"
-# the field holding the unique ID...
-field2 = "id"
+fields = ("facilitiesid","name","wkt",
+        "boatnr","capacity","facility_type",
+        "ifca","mpa","x",
+        "y","sail","commercial",
+        "other","fish","prop",
+        "mean_prop","mean_est")
 
-# set up the empty list...
+
 featureList = []
 
 
 cursor = arcpy.SearchCursor(fname)
 row = cursor.next()
 while row:
-    print (row.getValue(field2))
-   
-    WKT = row.getValue(field1)
+    print (row.getValue(fields[1]))
+
+    WKT = row.getValue(fields[2])
     # this is the part that converts the WKT string to geometry using the defined spatial reference...
-    temp = arcpy.FromWKT(WKT, sr)
+    geog = arcpy.FromWKT(WKT, sr)
     # append the current geometry to the list...
-    featureList.append(temp)
+    featureList.append(geog)
 
     row = cursor.next()
    
 # copy all geometries in the list to a feature class...
-arcpy.CopyFeatures_management(featureList, "C:\\Users\\Team\\Documents\\Theo Laptop Folder\\Tasks\\Quick tasks\\WKTtest\\WKTShapes.shp")   
+arcpy.CopyFeatures_management(featureList,
+    "C:/temp/facilities.shp")   
+
 
 # clean up...
-del row, temp, WKT, File, field1, featureList, cursor
+#del row, geog, WKT, File, field1, featureList, cursor
 
