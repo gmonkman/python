76ef1db4d10a355f604e445c8e21dfde68f616f2
Graham Monkman <gmonkman@mistymountains.biz>
2019-08-25 21:43:16 +0100

    work

--- mmo/scripts/survey_validate_overall.py
+++ mmo/scripts/survey_validate_overall.py
@@ -3,14 +3,13 @@
 from sklearn.metrics import mean_squared_error
 import numpy as np
 import random
-import mmodb
 import funclib.pandaslib as pandaslib
 import funclib.iolib as iolib
+import funclib.baselib as baselib
+CSV = 'C:/Users/Graham Monkman/OneDrive/Documents/MMOMapping/survey/_results/survey_validate_overall.py.csv'
 
-CSV = 'C:/Users/Graham Monkman/OneDrive/Documents/MMOMapping/survey/_results'
 
-
-TEST_NR = 100000
+TEST_NR = 10
 IFCA = ['northumberland', 'northumberland', 'northumberland', 'northumberland', 'northumberland', 'northumberland', 'northumberland', 'northumberland', 'northumberland', 'northumberland', 'northumberland', 'northumberland', 'northumberland', 'northumberland', 'northumberland', 'northumberland', 'north east', 'north east', 'north east', 'north east', 'north east', 'north east', 'north west', 'isles of scilly', 'isles of scilly', 'isles of scilly', 'isles of scilly', 'eastern', 'eastern', 'eastern', 'eastern', 'eastern', 'devon and severn', 'devon and severn', 'devon and severn', 'devon and severn', 'devon and severn', 'devon and severn', 'cornwall', 'cornwall', 'cornwall', 'cornwall', 'cornwall', 'north west', 'north west', 'sussex', 'sussex', 'sussex', 'sussex', 'sussex', 'kent and essex', 'kent and essex', 'kent and essex', 'kent and essex', 'kent and essex', 'kent and essex', 'kent and essex', 'kent and essex', 'southern', 'southern', 'southern', 'southern', 'southern', 'southern', 'north west', 'north west', 'north east', 'north east', 'north east', 'north east', 'north east', 'north east']
 OUR_RANK = [3, 2, 3, 2, 2, 3, 2, 2, 2, 2, 3, 2, 3, 2, 2, 2, 3, 3, 1, 3, 1, 2, 3, 2, 3, 3, 3, 3, 3, 3, 3, 1, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 2, 1, 3, 3, 3, 3, 1, 3, 1, 2]
 SURVEY_RANK = [3, 2, 3, 3, 2, 2, 3, 3, 3, 2, 2, 2, 3, 3, 3, 1, 1, 3, 1, 3, 1, 2, 3, 2, 1, 1, 1, 3, 2, 3, 3, 3, 3, 1, 3, 2, 1, 3, 1, 3, 3, 3, 3, 3, 1, 2, 3, 3, 3, 3, 3, 3, 2, 1, 2, 2, 2, 3, 2, 3, 3, 3, 3, 3, 3, 1, 3, 3, 2, 3, 1, 2]
@@ -29,7 +28,7 @@ def calc_dist(rank):
     assert isinstance(gb, pandaslib.GroupBy)     
     df_means = gb.execute()
     ifcas = list(gb.result.index)
-    means = list(gb.result.dist.mean)
+    means = baselib.list_flatten(gb.result.dist.values.tolist())
     assert len(ifcas) == len(means)
     return ifcas, means
 
@@ -43,28 +42,29 @@ def main():
 
     all_raw = []
     all_closer_agreement = []
-
+    PP = iolib.PrintProgress(TEST_NR)
     for _ in range(TEST_NR):
-        rank = [random.choose([1, 2, 3]) for x in range(len(OUR_RANK))]
-        for ifca, mean in zip(calc_dist(rank)):
+        rank = [random.choice([1, 2, 3]) for x in range(len(OUR_RANK))]
+        for ifca, mean in zip(*calc_dist(rank)):
             ifca_raw[ifca].append(mean)
-            ifca_closer_agreement.append(1 if actual_mean_distances[actual_ifcas.index(ifca)] < mean else 0)
+            ifca_closer_agreement[ifca].append(1 if actual_mean_distances[actual_ifcas.index(ifca)] > mean else 0)
         
         #Now do the overall
         means = [abs(OUR_RANK[i] - x) for i, x in enumerate(rank)]
         m = sum(means)/len(means)
         all_raw.append(m) #the random overall calculated mean distance value
         all_closer_agreement.append(1 if m < MEAN_DISTANCE else 0) #1 if the random calculated value is closer than ours
+        PP.increment()
+    #expected_mean_distance is 3/9*0 + 4/9*1 + 2/9*2 = 0.889
+    output = [['ifca', 'actual_mean_distance', 'expected_mean_distance', 'empirical_mean_distance', 'n', 'closer_n', 'p']]
 
-    output = [['ifca', 'mean_distance', 'n', 'closer_n', 'p']]
-
-    for k, v in ifca_raw.items():
-        output.append([k, actual_mean_distances[actual_ifcas.index(k)], TEST_NR, v.count(1), v.count(1)/TEST_NR])
+    for k, v in ifca_closer_agreement.items():
+        output.append([k, actual_mean_distances[actual_ifcas.index(k)], 0.889, sum(ifca_raw[k])/len(ifca_raw[k]), TEST_NR, v.count(1), v.count(1)/TEST_NR])
 
     #add the overall record
     f = all_closer_agreement.count(1)
-    output.append(['all', MEAN_DISTANCE, TEST_NR, f, f/TEST_NR])
-    iolib.write_csv(CSV, output, inner_as_rows=False)
+    output.append(['all', MEAN_DISTANCE, 0.889, sum(all_raw)/len(all_raw), TEST_NR, f, f/TEST_NR])
+    iolib.writecsv(CSV, output, inner_as_rows=False)
     print('\nResults saved to: %s' % CSV)
 
 
